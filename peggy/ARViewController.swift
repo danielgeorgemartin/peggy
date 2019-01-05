2d9af4b3416653e1ce76728c94a672b4db62a1dc
Daniel Martin <danielmartin@daniels-macbook-pro.local>
2019-01-05 21:52:31 +0000

    Peggy Version 1.8 - Improved AR functionality - Implemented feature points in AR Scene - Implemented Apple grid.png in AR Scene - Altered plane detection - Altered touch to add 3D model, with 4 Matrix points for accuracy - Altered 3D model scaling and orientation - Removed AR add and reset buttons

--- peggy/ARViewController.swift
+++ peggy/ARViewController.swift
@@ -14,16 +14,22 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet weak var sceneView: ARSCNView!
     
+    let configuration = ARWorldTrackingConfiguration()
+    
     private var hud : MBProgressHUD!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+        
         // load in textures for 3D model
         self.sceneView.autoenablesDefaultLighting = true
         
         self.hud = MBProgressHUD.showAdded(to: self.sceneView, animated: true)
-        self.hud.label.text = "Detecting Peggy"
+        self.hud.label.text = "Detecting Plane"
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -41,12 +47,40 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
         
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView)
+            
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            
+            if let hitResults = results.first {
+                
+                let peggyScene = SCNScene(named: "peggy.scn")!
+                
+                if let peggyNode = peggyScene.rootNode.childNode(withName: "peggy", recursively: true) {
+                    
+                    peggyNode.position = SCNVector3(x: hitResults.worldTransform.columns.3.x, y: hitResults.worldTransform.columns.3.y, z: hitResults.worldTransform.columns.3.z)
+                    
+                    sceneView.scene.rootNode.addChildNode(peggyNode)
+                    
+                }
+
+            }
+        }
+    }
+    
+    func restartSession() {
+        self.sceneView.session.pause()
+        self.sceneView.scene.rootNode.enumerateChildNodes { (peggyNode, _) in
+            peggyNode.removeFromParentNode()
+        }
+        self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    
     
     // declare function that recognises that the users taps to drop Peggy onto plane
     private func registerGestureRecognisers(){
-        
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
-        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
         
         let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(pinched))
         
@@ -86,50 +120,39 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
         
     }
     
-    // implement tap function
-    @objc func tapped(recogniser : UITapGestureRecognizer) {
-        
-        // get coordinates of where the user has tapped
-        guard let sceneView = recogniser.view as? ARSCNView else {
-            return
-        }
-        
-        // recognise where user has touched in sceneView
-        let touch = recogniser.location(in: sceneView)
-        
-        // check whether location of touch intersects with horizontal scene plane
-        let hitTestResults = sceneView.hitTest(touch, types: .existingPlane)
-        
-        // takes first item of hit test array to check whether touch has occurred and is valid
-        if let hitTest = hitTestResults.first {
-            
-            let peggyScene = SCNScene(named: "peggy.scn")!
-            
-            guard let peggyNode = peggyScene.rootNode.childNode(withName: "peggy", recursively: true) else {
-                return
-            }
-            
-            // use 3D coordinates that user has touched on the plane
-            peggyNode.position = SCNVector3(hitTest.worldTransform.columns.3.x, hitTest.worldTransform.columns.3.y, hitTest.worldTransform.columns.3.z)
-            
-            self.sceneView.scene.rootNode.addChildNode(peggyNode)
-            
-        }
-    }
-    
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         
         // check that the correct plane is detected
         if anchor is ARPlaneAnchor {
             
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            let planeNode = SCNNode()
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            
+            // tip vertical plane to be horizontal
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
+            
+            let gridMaterial = SCNMaterial()
+            
+            gridMaterial.diffuse.contents = #imageLiteral(resourceName: "grid.png")
+            plane.materials = [gridMaterial]
+            
+            planeNode.geometry = plane
+            
+            node.addChildNode(planeNode)
+            
             DispatchQueue.main.sync {
-                self.hud.label.text = "Peggy Detected"
+                self.hud.label.text = "Plane Detected"
                 self.hud.hide(animated: true, afterDelay: 0.5)
             }
             
         }
     }
+
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
